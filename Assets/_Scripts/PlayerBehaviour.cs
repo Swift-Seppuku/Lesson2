24283c8c9ac75b472f01414328a49107ca620024
Walid Hassan <Walidhassan725@gmail.com>
2021-01-25 18:21:24 -0500

    added cameracontroller

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float movementForce;
+    public float jumpForce;
     public Rigidbody rigidBody;
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,18 +20,65 @@ public class PlayerBehaviour : MonoBehaviour
     // approx updates 60 times per second = 60 fps
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0)
+        if (isGrounded) 
         {
-            //move right
-            //Debug.Log("moving Right");
-            rigidBody.AddForce(Vector3.right * movementForce);
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                //move right
+                //Debug.Log("moving Right");
+                rigidBody.AddForce(Vector3.right * movementForce);
 
+            }
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                // move left
+                //Debug.Log("Moving Left");
+                rigidBody.AddForce(Vector3.left * movementForce);
+            }
+
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                //move right
+                //Debug.Log("moving forward");
+                rigidBody.AddForce(Vector3.forward * movementForce);
+
+            }
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                // move left
+                //Debug.Log("Moving back");
+                rigidBody.AddForce(Vector3.back * movementForce);
+            }
+            if (Input.GetAxisRaw("Jump") > 0)
+            {
+                //jump
+                rigidBody.AddForce(Vector3.up * jumpForce);
+            }
+                
         }
-        if (Input.GetAxisRaw("Horizontal") < 0)
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
         {
-            // move left
-            //Debug.Log("Moving Left");
-            rigidBody.AddForce(Vector3.left * movementForce);
+            isGrounded = true;
+        }
+        
+    }
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+   
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
         }
     }
 }
