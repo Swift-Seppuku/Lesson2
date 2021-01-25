22305aa30da4b7795e75227804819d16fe8140ea
Walid Hassan <Walidhassan725@gmail.com>
2021-01-25 17:29:38 -0500

    some scripting added

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    public float movementForce;
+    public Rigidbody rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    
+    // Update is called once per frame - once every 16.6666ms
+    // 1000ms for each second
+    // approx updates 60 times per second = 60 fps
     void Update()
     {
-        
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            //move right
+            //Debug.Log("moving Right");
+            rigidBody.AddForce(Vector3.right * movementForce);
+
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            // move left
+            //Debug.Log("Moving Left");
+            rigidBody.AddForce(Vector3.left * movementForce);
+        }
     }
 }
